6f85fe89e18f8ca90ea7eb321f496335fe512f4e
MikayelArakelyan <099066664m@gmail.com>
2023-08-26 22:09:11 +0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 1000, b = 5555, c = 1892;
+        if (a == 1 || b == 1 || c == 1) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
+        System.out.println((a == 1 || b == 1 || c == 1) ? true : false);
+        if (a%2 != 0) {
+
+        }
+
+    }
+}
